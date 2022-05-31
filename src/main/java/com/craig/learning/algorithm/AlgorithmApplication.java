ae65e0e0c04d5d2bff6bab1f6cfc0720c09efbb7
Craig.Duan <atawat@live.cn>
2022-05-31 21:29:07 +0800

    初始化项目
    and

--- src/main/java/com/craig/learning/algorithm/AlgorithmApplication.java
+++ src/main/java/com/craig/learning/algorithm/AlgorithmApplication.java
@@ -1,9 +1,5 @@
 package com.craig.learning.algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,22 +8,5 @@ public class AlgorithmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlgorithmApplication.class, args);
-    }
-
-
-    public void printA(){
-        List<Integer> arrayList = new ArrayList<>();
-        findTarget(arrayList, (Integer i)-> i == 5, ()->{
-            System.out.println("a");
-        });
-
-    }
-
-    public <T> void findTarget(Iterable<T> source, Predicate<T> filter, Runnable action){
-        source.forEach(i->{
-            if(filter.test(i)){
-                action.run();
-            }
-        });
     }
 }
